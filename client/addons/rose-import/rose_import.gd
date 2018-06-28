e6686ec9ea24b4e4202debf866e3ea5094764797
Ralph Minderhoud <ralphminderhoud@gmail.com>
2018-06-28 12:20:24 -0500

    Reorganize files

--- client/addons/rose-import/rose_import.gd
+++ client/addons/rose-import/rose_import.gd
@@ -6,17 +6,17 @@ var zmo_importer = null
 var zms_importer = null
 
 func _enter_tree():
-	zmd_importer = preload("importer_zmd.gd").new()
-	zmo_importer = preload("importer_zmo.gd").new()
-	zms_importer = preload("importer_zms.gd").new()
-	add_import_plugin(zmd_importer)
-	add_import_plugin(zmo_importer)
-	add_import_plugin(zms_importer)
+    zmd_importer = preload("importers/zmd_importer.gd").new()
+    zmo_importer = preload("importers/zmo_importer.gd").new()
+    zms_importer = preload("importers/zms_importer.gd").new()
+    add_import_plugin(zmd_importer)
+    add_import_plugin(zmo_importer)
+    add_import_plugin(zms_importer)
 
 func _exit_tree():
-	remove_import_plugin(zmd_importer)
-	remove_import_plugin(zmo_importer)
-	remove_import_plugin(zms_importer)
-	zmd_importer = null
-	zmo_importer = null
-	zms_importer = null
+    remove_import_plugin(zmd_importer)
+    remove_import_plugin(zmo_importer)
+    remove_import_plugin(zms_importer)
+    zmd_importer = null
+    zmo_importer = null
+    zms_importer = null
---
+++ client/addons/rose-import/utils/utils.gd
@@ -0,0 +1,9 @@
+tool
+
+static func r2g_position(pos):
+    # Converts ROSE (Z-UP) position to Godot (Y-UP) position
+    return Vector3(pos.x, pos.z, pos.y)
+
+static func r2g_rotation(rot):
+    # Converts ROSE (Z-UP) quaternion to Godot (Y-UP) quaternion
+    return Quat(rot.x, rot.z, rot.y, -rot.w)
